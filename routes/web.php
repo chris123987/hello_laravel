6481c0bb34817dc57ca4ccecf7a83f268ec3edaa
Chris <605313104@qq.com>
2018-05-09 07:51:14 +0000

    修改路由注释

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,12 @@
 //Route::get('/', function () {
 //    return view('/static_pages/home');
 //});
-
+//首页
 Route::get('/','Test\StaticPagesController@home')->name('home');
 Route::get('/help','Test\StaticPagesController@help')->name('help');
-
+//注册页面
 Route::get('/signup','Test\UserController@create')->name('signup');
-
+//处理注册提交的页面并跳转
 //Route::resource('users','Test\UserController');
 Route::get('/users/{user}','Test\UserController@show')->name('users.show');
 Route::any('/store','Test\UserController@store')->name('store');
